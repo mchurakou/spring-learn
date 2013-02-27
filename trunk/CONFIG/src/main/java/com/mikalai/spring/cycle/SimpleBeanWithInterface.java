49965352416b0cc0020b67b99da2f7675b77229f
1badbug1@gmail.com <1badbug1@gmail.com@a5f5def9-a616-71d7-4398-db94dd2e8363>
2013-02-27 18:31:04 +0000

    Config chapter was started

---
+++ trunk/CONFIG/src/main/java/com/mikalai/spring/cycle/SimpleBeanWithInterface.java
@@ -0,0 +1,67 @@
+package com.mikalai.spring.cycle;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class SimpleBeanWithInterface implements InitializingBean {
+    private static final String DEFAULT_NAME = "Luke Skywalker";
+    private String name = null;
+    private int age = Integer.MIN_VALUE;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+
+    
+    private static SimpleBeanWithInterface getBean(String beanName, ApplicationContext ctx){
+        try{
+            SimpleBeanWithInterface bean = (SimpleBeanWithInterface) ctx.getBean(beanName);
+            System.out.println(bean);
+            return bean;
+        } catch (BeanCreationException e){
+            System.out.println("Error" + e.getMessage());
+            return null;
+        }
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "SimpleBean [name=" + name + ", age=" + age + "]";
+    }
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:app-context.xml");
+        ctx.refresh();
+        
+        SimpleBeanWithInterface sb1 = getBean("simpleBeanWintInterface1", ctx);
+        SimpleBeanWithInterface sb2 = getBean("simpleBeanWintInterface2", ctx);
+        SimpleBeanWithInterface sb3 = getBean("simpleBeanWintInterface3", ctx);
+
+    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Init");
+        if (name == null){
+            System.out.println("Using def name");
+            name = DEFAULT_NAME;
+        }
+        
+        if (age == Integer.MIN_VALUE){
+            throw new IllegalArgumentException("Set age for " + SimpleBeanWithInterface.class);
+        }
+
+    }
+
+}
