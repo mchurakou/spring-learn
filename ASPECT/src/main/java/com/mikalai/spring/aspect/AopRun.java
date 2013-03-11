f80aba0a54bc7025b9197ca198b72d55e5e020c2
1badbug1@gmail.com <1badbug1@gmail.com@a5f5def9-a616-71d7-4398-db94dd2e8363>
2013-03-11 19:52:05 +0000

    -AOP chapter

---
+++ ASPECT/src/main/java/com/mikalai/spring/aspect/AopRun.java
@@ -0,0 +1,26 @@
+package com.mikalai.spring.aspect;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+public class AopRun {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        MessageWriter target = new MessageWriter();
+        
+        ProxyFactory pf = new ProxyFactory();
+        pf.addAdvice(new MessageDecorator());
+        pf.setTarget(target);
+        
+        MessageWriter proxy = (MessageWriter)pf.getProxy();
+        
+        target.writeMessage();
+        
+        System.out.println();
+        proxy.writeMessage();
+
+    }
+
+}
---
+++ ASPECT/src/main/java/com/mikalai/spring/aspect/MessageWriter.java
@@ -0,0 +1,7 @@
+package com.mikalai.spring.aspect;
+
+public class MessageWriter {
+    public void writeMessage(){
+        System.out.print("World");
+    }
+}
