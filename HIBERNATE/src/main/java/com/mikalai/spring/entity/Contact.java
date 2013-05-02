857fc17ea3afa73b710418d6e0778af57b26176a
1badbug1@gmail.com <1badbug1@gmail.com@a5f5def9-a616-71d7-4398-db94dd2e8363>
2013-05-02 18:12:58 +0000

    -hibernate

---
+++ HIBERNATE/src/main/java/com/mikalai/spring/entity/Contact.java
@@ -0,0 +1,114 @@
+package com.mikalai.spring.entity;
+
+import java.io.Serializable;
+
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+
+
+@Entity
+@Table(name="contact")
+public class Contact implements Serializable {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private int version;
+
+    private Set<ContactTelDetail> contactTelDetails = new HashSet<ContactTelDetail>();
+    private Set<Hobby> hobbies = new HashSet<Hobby>();
+    
+    
+    @ManyToMany
+    @JoinTable(name="contact_hobby_detail", joinColumns = @JoinColumn(name="CONTACT_ID"),
+    inverseJoinColumns = @JoinColumn(name="HOBBY_ID"))
+    public Set<Hobby> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(Set<Hobby> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [id=" + id + ", firstName=" + firstName + ", lastName="
+                + lastName + ", birthDate=" + birthDate
+                + ", contactTelDetails=" + /*contactTelDetails +*/ "]";
+    }
+    
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name="ID")
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    @Column(name="FIRST_NAME")
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    @Column(name="LAST_NAME")
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name="BIRTH_DATE")
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(Date date) {
+        this.birthDate = date;
+    }
+    
+    @OneToMany(mappedBy="contact", cascade=CascadeType.ALL, orphanRemoval=true)
+    public Set<ContactTelDetail> getContactTelDetails() {
+        return contactTelDetails;
+    }
+    public void setContactTelDetails(Set<ContactTelDetail> contactTelDetails) {
+        this.contactTelDetails = contactTelDetails;
+    }
+    
+    @Version
+    @Column(name="VERSION")
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+    
+    public void addContactTelDetails(ContactTelDetail contactTelDetail){
+        
+        contactTelDetail.setContact(this);
+        getContactTelDetails().add(contactTelDetail);
+    }
+    
+    public void removeContactTelDetails(ContactTelDetail contactTelDetail){
+        
+        
+        getContactTelDetails().remove(contactTelDetail);
+    }
+
+
+}
