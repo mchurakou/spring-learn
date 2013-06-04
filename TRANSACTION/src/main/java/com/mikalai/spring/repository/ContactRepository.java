c92c08c07add4f935069b665554f1a72020e1df9
1badbug1@gmail.com <1badbug1@gmail.com@a5f5def9-a616-71d7-4398-db94dd2e8363>
2013-06-04 18:55:46 +0000

    -TRANSACTION via annotation

--- TRANSACTION/src/main/java/com/mikalai/spring/repository/ContactRepository.java
+++ TRANSACTION/src/main/java/com/mikalai/spring/repository/ContactRepository.java
@@ -6,9 +6,10 @@ package com.mikalai.spring.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.mikalai.spring.domain.Contact;
-
+import org.springframework.data.jpa.repository.Query;
 
 public interface ContactRepository extends CrudRepository<Contact, Long> {
-
+    @Query("select count(c) from Contact c")
+    public Long countAllContacts();
 	
 }
