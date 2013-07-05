3378b4b2dc339fe3eda5e88b6b15a1b60c8108f5
1badbug1@gmail.com <1badbug1@gmail.com@a5f5def9-a616-71d7-4398-db94dd2e8363>
2013-07-05 16:43:53 +0000

    -TESTING: module testing

---
+++ MVC/src/test/java/com/mikalai/mvc/test/config/ControllerTestConfig.java
@@ -0,0 +1,13 @@
+
+package com.mikalai.mvc.test.config;
+
+import org.junit.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Configuration
+@Profile("test")
+public class ControllerTestConfig {
+	
+}
